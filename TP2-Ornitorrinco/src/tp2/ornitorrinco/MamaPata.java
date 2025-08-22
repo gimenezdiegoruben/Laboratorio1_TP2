92af028e41026eda2796e1da16c22d2f522e491b
Ger Mecias <carlosgermanmecias2002@gmail.com>
2025-08-22 18:05:05 -0300

    Agrega interface MamaPata mas metodo tocarOrgano() y atributos pico y pata

--- TP2-Ornitorrinco/src/tp2/ornitorrinco/MamaPata.java
+++ TP2-Ornitorrinco/src/tp2/ornitorrinco/MamaPata.java
@@ -3,5 +3,9 @@ package tp2.ornitorrinco;
 /** GRUPO 6 */
 
 public interface MamaPata {
+    double CENTIMETROS_PICO= 6.8; //CM
+    double CENTIMETROS_PATAS= 7.5; //CM
+    
+    void tocarOrgano(); //Implementado por orniVerde
     
 }
