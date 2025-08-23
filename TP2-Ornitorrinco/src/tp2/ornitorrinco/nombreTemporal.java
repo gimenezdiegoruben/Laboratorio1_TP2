2ce456fc5b884ae05b747a6c7d06d703946e1dcf
Lucas Saidman <saidmanlucas.p@gmail.com>
2025-08-23 17:50:35 -0300

    Corrección de nombre

--- TP2-Ornitorrinco/src/tp2/ornitorrinco/nombreTemporal.java
+++ TP2-Ornitorrinco/src/tp2/ornitorrinco/nombreTemporal.java
@@ -2,7 +2,7 @@ package tp2.ornitorrinco;
 import java.util.Scanner;
 /** GRUPO 6 */
 
-public interface mamaPata {
+public interface nombreTemporal {
     double CENTIMETROS_PICO= 6.8; //CM
     double CENTIMETROS_PATAS= 7.5; //CM
     
