c9c6e5e4ca1976054d3d88499adbf99c8898f8bb
Lucas Saidman <saidmanlucas.p@gmail.com>
2025-08-23 17:32:53 -0300

    Actualizacion nombre
    Cambio de MamaPata a mamaPata por consigna

--- TP2-Ornitorrinco/src/tp2/ornitorrinco/MamaPata.java
+++ TP2-Ornitorrinco/src/tp2/ornitorrinco/MamaPata.java
@@ -2,7 +2,7 @@ package tp2.ornitorrinco;
 import java.util.Scanner;
 /** GRUPO 6 */
 
-public interface MamaPata {
+public interface mamaPata {
     double CENTIMETROS_PICO= 6.8; //CM
     double CENTIMETROS_PATAS= 7.5; //CM
     
