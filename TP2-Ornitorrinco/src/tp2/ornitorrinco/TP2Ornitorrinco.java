da588b13dbd035b98deaf0490dfbcde24970e9c3
Ignacio Rodriguez Scarso <nacho.rodriguez162@gmail.com>
2025-08-26 02:34:56 -0300

    Modifique Main
    Completando Main

--- TP2-Ornitorrinco/src/tp2/ornitorrinco/TP2Ornitorrinco.java
+++ TP2-Ornitorrinco/src/tp2/ornitorrinco/TP2Ornitorrinco.java
@@ -56,5 +56,78 @@ public class TP2Ornitorrinco {
         } while(propBlue < 5 || propBlue > 10);
       
         orniAzul blue = new orniAzul(propBlue, velocidad);
+    
+            System.out.println("Creamos el OrniAzul Blui:");
+        int propBlui = 0;
+        
+        do{
+            
+            System.out.println("Velocidad de Propulsion: (entre 5 y 10 Km/s)");
+            
+            if(sc.hasNextInt()){
+                propBlui = sc.nextInt();
+                sc.nextLine();
+                
+                if(propBlui < 5 || propBlui > 10){
+                System.out.println("ERROR: la velocidad debe ser entre 5 y 10 Km/s");
+                }
+            } else {
+                System.out.println("Numero invalido, intente de nuevo");
+                sc.next();
+                propBlui = -1;
+            }
+            
+        } while(propBlui < 5 || propBlui > 10);
+        
+        orniAzul blui = new orniAzul(propBlui, velocidad);
+
+        System.out.println("--- HABILIDADES ---");
+        System.out.println("OrniVerde:");
+        verde.nadar();
+        verde.tocarGuitarra();
+        verde.tocarOrgano(sc);
+        verde.tocarGuitorgan(sc);
+
+        System.out.println("OrniAzul Blue:");
+        blue.nadar();
+        blue.tocarGuitarra();
+
+        System.out.println("OrniAzul Blui:");
+        blui.nadar();
+        blui.tocarGuitarra();
+
+        System.out.println("Carga en el Array");
+        try {
+            Object[] hermanos = new Object[3];
+            hermanos[0] = blue;
+            hermanos[1] = verde;
+            hermanos[2] = blui;
+
+            System.out.println("Lista de nadadores (solo orniAzul)");
+            List<orniAzul> nadadores = new ArrayList<>();
+            for (Object obj : hermanos) {
+                if (obj instanceof orniAzul) {
+                    nadadores.add((orniAzul) obj);
+                }
+            }
+
+            nadadores.sort(orniAzul.porPropulsionAsc);
+
+            System.out.println("Nadadores ordenados por propulsion (km/s) ascendente:");
+            Iterator<orniAzul> it = nadadores.iterator();
+            while (it.hasNext()) {
+                orniAzul o = it.next();
+                System.out.println(o.toString());
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error de índice en el arreglo: " + e.getMessage());
+        } finally {
+            System.out.println("los ornitohermanos estan juntos al fin...");
+        }
+
+        sc.close();
+        
     }
+    
 }
