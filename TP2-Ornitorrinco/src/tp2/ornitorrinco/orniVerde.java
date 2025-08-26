9bea358f4fb6c5f9b8ccb5dcc84ba65407bce24f
Lucas Saidman <saidmanlucas.p@gmail.com>
2025-08-25 23:25:59 -0300

    Clase orniVerde
    Hecha por Tomas Migliozzi, no pudo hacer el push el por que Diego no lo agrego al repositorio.

--- TP2-Ornitorrinco/src/tp2/ornitorrinco/orniVerde.java
+++ TP2-Ornitorrinco/src/tp2/ornitorrinco/orniVerde.java
@@ -1,7 +1,37 @@
 package tp2.ornitorrinco;
+import java.util.Scanner;
 
 /** GRUPO 6 **/
+/* Hecho Por Tomas Migliozzi */
 
-public class orniVerde {
+public class orniVerde extends papaCastor implements mamaPata{
+    private String nombre;
+
+    public OrniVerde(String nombre,double cola , double velocidad) {
+        super(cola, velocidad);
+        this.nombre = nombre;
+    }
     
+    public void tocarOrgano(Scanner sc){
+        System.out.println("Do-Re-Mi...");
+        System.out.println("Presione cualquier enter para continuar");
+        sc.nextLine();
+        System.out.println("Fa-Sol-La-Si");
+    }
+    
+    public void tocarGuitorgan(Scanner sc){
+        super.tocarGuitarra();
+        tocarOrgano(sc);
+        System.out.println("cuac cuaac....");
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    @Override
+        public void nadar(){
+        super.nadar();
+    }
 }
